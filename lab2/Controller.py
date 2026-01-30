bcf360904c884e9b3c0abbee00f4e94c33659c78
Mackenzie <mack.beck977@gmail.com>
2026-01-30 08:29:27 -0700

    initial commit for lab2

---
+++ lab2/Controller.py
@@ -0,0 +1,287 @@
+"""
+Controller to handle all user inputs.
+Interacts with the Model.
+Alters the view .
+"""
+
+class Controller:
+
+    View = None
+    Model = None
+
+
+
+    def create_new_db(self):
+        self.Model.create_new_db()
+
+    def is_db_empty(self):
+        
+        with open(self.Model.database_file_name, "r") as f: 
+            if not f.read(1):
+                print("Database is currently empty!")
+                return True
+            return False
+
+    def handle_file_not_found(self):
+        print("Database not found, would you like to create one? (Y/N)")
+        sel = input()
+        if sel.lower() == "y":
+            self.create_new_db()
+            self.View.main_menu()
+        if sel.lower() == "n":
+            print("No database provided, exiting program")
+
+    def parse_line(self, line: str):
+        # returns a list of the data elements for a single row in the database
+        tmp = line.strip()
+        data_list = tmp.split(":")
+        #print("Parsed line data: ", data_list)
+        return data_list
+
+    def print_employee(self, data):
+        print("Employee ID: ", data[0])
+        print("\nEmployee name:", data[1], data[2])
+        print("\nDepartment:", data[3])
+
+    def get_employee_data_by_id(self, id_num : int):
+        """return employee data for a row specified by employee id"""
+
+        #print("Searching for employee ID:", id_num)
+        try:
+            with open(self.Model.database_file_name, "r") as f:
+                for line in f:
+                    data = self.parse_line(line)
+                    #print("data[0]:", data[0])
+                    #print("id_num:", id_num)
+                    #print(data[0] == id_num)
+
+                    if data[0] == id_num:
+                        return data
+                return None
+        except FileNotFoundError:
+            self.handle_file_not_found()
+
+
+
+    def is_id_num_valid_number(self, id_num:str):
+                # check if employee id is a valid int
+        #print("is_id_num_valid")
+        try:
+            int(id_num)
+        except ValueError:
+            print("Employee ID must be a number, please re-enter: ")
+            return False
+        if int(id_num) < 0:
+            print("Employee Id must be a positive integer, please re-enter: ")
+            return False
+        if len(id_num) != 4:
+            print("Employee ID must be 4 digits long, please re-enter: ")
+            return False
+        return True
+
+    def is_id_num_valid(self, id_num :str):
+        # check if employee id is a valid int
+        #print("is_id_num_valid")
+        if not self.is_id_num_valid_number(id_num):
+            return False
+
+        # check if employee already exists
+
+        data = self.get_employee_data_by_id(id_num)
+
+        if data:
+            print("Employee already exists! Please enter a different employee ID: ")
+            return False
+        
+        return True
+
+
+
+    def add_new_employee(self):
+        """
+        get user input
+        check if employee already exists
+        check if department exists
+        open file as append use with statement
+        append line of text to file in correct format
+        ask user if they want to enter another employee or go back to main
+        """
+
+        print("Enter employee ID number: ")
+        id_num = input()
+
+        while not self.is_id_num_valid(id_num):
+            id_num = input()
+
+
+
+
+
+
+
+        print("\nEnter employee first name: ")
+        fname = input()
+
+        #check if fname is a string
+
+        if not fname.isalpha():
+            print("First name must be a string.")
+            self.add_new_employee()
+
+        print("\nEnter employee last name: ")
+        lname = input()
+
+        # check if lname is a vlaid string
+        if not lname.isalpha():
+            print("Last name must be a string.")
+            self.add_new_employee()
+
+
+
+
+
+
+
+        print("\nEnter employee department")
+        department = input()
+
+        # Check if department exists
+
+        if department not in self.Model.valid_departments:
+            print("Department: ", department, " does not exist. Use one of the following: ")
+            print(self.Model.valid_departments)
+            self.add_new_employee()
+        else:
+            print("Adding new record to database:\n")
+            new_record = id_num + ":" + fname + ":" + lname + ":" + department
+            try:
+                with open(self.Model.database_file_name, "a") as f:
+                    f.write(new_record+ '\n')
+            except FileNotFoundError:
+                self.handle_file_not_found()
+
+
+
+
+
+        print("\nWould you like to add another record? (Y/N): ")
+        inp = input()
+        tmp_inp = inp.lower()
+
+        if tmp_inp == "y":
+            self.add_new_employee()
+        elif tmp_inp == "n":
+            self.View.main_menu()
+        else:
+            print("\nReturning to main menu.")
+
+    def search_employee(self):
+        """
+        Get employee id 
+        open file as read, read through every line checking for the entered id, if not present print error and return null
+        if present return the data
+        """
+
+        print("Enter employee ID number: ")
+        id_num = input()
+        while not self.is_id_num_valid_number(id_num):
+            id_num = input()
+
+        data = self.get_employee_data_by_id(id_num)
+
+        if self.is_db_empty():
+            self.View.main_menu()
+        if data:
+            print("Employee found!")
+            print("--------------------\n")
+            self.print_employee(data)
+
+            print("\n\n")
+
+            print("If you want to search for another employee, enter 1.\n")
+            print("If you want to return to the main menu, enter 2.\n")
+            inp = input()
+            if inp == "1":
+                self.search_employee()
+            if inp == "2":
+                self.View.main_menu()
+            else:
+                print("/Now thats not a number like I said but I'll just take you back to the main menu.\n")
+                self.View.main_menu()
+        else:
+            print("Employee not found. Please try again.")
+            self.search_employee()
+            
+
+
+    def remove_employee(self):
+        print("Enter ID of employee you wish to remove: ")
+        id_num = input()
+        employee_records = []
+
+        while not self.is_id_num_valid_number(id_num):
+            id_num = input()
+
+        if self.is_db_empty():
+            self.View.main_menu()
+
+        if not self.get_employee_data_by_id(id_num):
+            print("Employee", id_num, "not found")
+            self.remove_employee()
+        else:
+            print("Are you sure you want to delete employee:", id_num, "(Y/N)")
+            response = input()
+            if response.lower() == 'y':
+                print("Deleting employee record from database")
+                try:
+                    with open(self.Model.database_file_name, "r") as f:
+                        for line in f:
+                            line_data = self.parse_line(line)
+                            if line_data[0] == id_num:
+                                continue
+                            else:
+                                employee_records.append(line_data)
+
+                    # clear the existing file content and add records
+                    with open(self.Model.database_file_name, "w") as f:
+                        for record in employee_records:
+                            new_line = record[0] + ":" + record[1] + ":" + record[2] + ":" + record [3]
+                            f.write(new_line + "\n")
+                except FileNotFoundError:
+                    self.handle_file_not_found()
+                self.View.main_menu()
+
+
+
+            
+
+    def display_employees(self):
+        print("Display employees:\n")
+        try:
+            with open(self.Model.database_file_name, "r") as f:
+                self.is_db_empty()
+                for line in f:
+                    self.print_employee(self.parse_line(line))
+                    print("\n-----")
+
+                self.View.main_menu()
+        except FileNotFoundError:
+            self.handle_file_not_found()
+
+    def exit_program(self):
+        print("Exiting Program\n")
+        print("-----------------")
+        exit()
+
+    def handle_input(self, selection : str):
+        match selection:
+            case "1":
+                self.add_new_employee()
+            case "2":
+                self.search_employee()
+            case "3":
+                self.remove_employee()
+            case "4":
+                self.display_employees() 
+            case "5":
+                self.exit_program()
---
+++ lab2/Model.py
@@ -0,0 +1,47 @@
+"""
+Model to represent the data
+
+
+"""
+
+
+class Model:
+
+    valid_departments = ["Science", "Art", "Math", "English"]
+
+    database_file_name = "data.txt"
+
+
+    def create_new_db(self):
+        with open(self.database_file_name, "w"):
+            pass
+
+    def set_database_file_name(self, new_name:str):
+
+        # make sure db name is a valid string
+
+        if not new_name.isalpha():
+            print("Invalid file name")
+        else:
+            self.database_file_name = new_name
+
+    def get_database_file_name(self)-> str:
+        return self.database_file_name
+
+    def add_valid_department(self, department_name:str):
+
+        # make sure department name is a valid string
+        if not department_name.isalpha():
+            print("Invalid department name")
+        else:
+            self.valid_departments.append(department_name)
+            print(f"Department {department_name} added successfully.")
+
+
+    def remove_valid_department(self, department_name:str):
+
+        if department_name in self.valid_departments:
+            self.valid_departments.remove(department_name)
+            print(f"Department {department_name} removed successfully.")
+        else:
+            print(f"Department {department_name} not found in valid departments.")
